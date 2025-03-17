4fb4d8ea12369769d6a5cf783a410ec3075e58d2
ARINDAM GHOSH <128517088+arindam2590@users.noreply.github.com>
2025-03-17 19:26:24 +0530

    Add files via upload

--- Simulation/Utils/utils.py
+++ Simulation/Utils/utils.py
@@ -14,3 +14,34 @@ def setup_parser():
                         help='Simulate the environment with Dueling DQN Model: use argument -u or --dueldqn')
     args = parser.parse_args()
     return args
+    
+class DataVisualization:
+    def __init__(self, episodes, result, model, train_data_filename):
+        param_dir = 'Simulation/Utils/'
+        with open(param_dir + 'config.json', 'r') as file:
+            self.params = json.load(file)
+        self.model = model
+        self.fig_dir = self.params['DATA_DIR']
+        if not os.path.exists(self.fig_dir):
+            os.makedirs(self.fig_dir)
+        self.train_data_filename = train_data_filename
+        self.n_episodes = episodes
+        self.returns = result[0]
+        self.epsilon_decay_history = result[1]
+        self.training_error = result[2]
+        self.steps = result[3]
+        
+            
+    def save_data(self):
+        filepath = self.fig_dir + self.train_data_filename
+        df = pd.DataFrame({'Rewards': self.returns,
+                           'Steps': self.steps,
+                           'Epsilon Decay': self.epsilon_decay_history,
+                           'Training Error': self.training_error})
+        if not os.path.isfile(filepath):
+            with pd.ExcelWriter(filepath, mode='w') as writer:
+                df.to_excel(writer, sheet_name=self.model)
+                
+        else:
+            with pd.ExcelWriter(filepath, mode='a') as writer:
+                df.to_excel(writer, sheet_name=self.model)
